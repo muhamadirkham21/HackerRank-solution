4d20d681efbf7f4bbd3ee11c413804ccbff21982
irkham <muhirkham21@students.unnes.ac.id>
2023-10-06 11:05:50 +0700

    largest_perimeter_triangle.py

---
+++ LeetCode/largest_perimeter_triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums, reverse=True)
+        for i in range(len(nums)-2):
+            if nums[i] < nums[i+1] + nums[i+2]:
+                return nums[i] + nums[i+1] + nums[i+2]
+        return 0
